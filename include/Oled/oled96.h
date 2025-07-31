6c6b09ff38c296169a49252a1301e16bcfc7fe7b
LePhanQuocVu <vulephanquoc@gmail.com>
2025-07-31 11:56:15 +0700

    oled header

---
+++ include/Oled/oled96.h
@@ -0,0 +1,19 @@
+#ifndef OLED_96_H
+#define OLED_96_H
+
+
+#include <Adafruit_SSD1306.h>
+#include <Adafruit_GFX.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 32 // OLED display height, in pixels
+#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+
+extern Adafruit_SSD1306 display;
+void initOled();
+
+void displayTemperature();
+
+void TaskDisplayOled(void *pvParameter);
+#endif 
