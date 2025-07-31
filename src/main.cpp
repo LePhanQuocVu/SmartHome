4e630a0a8ee50bcef5cb39c6ccfae68635deeb75
LePhanQuocVu <vulephanquoc@gmail.com>
2025-07-31 11:59:57 +0700

    add file config

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,9 @@
 #include <Arduino.h>
 #include "../include/Wifi/wifi_connection.h"
+#include "../include/Sensor/dht11.h"
+#include "../include/Oled/oled96.h"
+
+
 
 #define LED_PIN 2
 #define LED_WIFI 4
@@ -19,12 +23,14 @@ void TaskConnectMqtt(void *pvParameter) {
 
 }
 
-
 void setup() {
   Serial.begin(115200);
+  initOled();
   connectWifi();
   xTaskCreate(TaskBlinkLED, "BlinkLED", 1024, NULL, 1, NULL);
   xTaskCreate(WifiMonitorTask, "ReconnectWifi", 4096, NULL,2,NULL);
+  xTaskCreate(TaskTemperature, "ReadDHT11", 2048, NULL,1,NULL);
+  xTaskCreate(TaskDisplayOled,"Oled",2048, NULL,1,NULL );
 }
 
 
