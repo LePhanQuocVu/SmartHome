60aed588e4d654bc909f9fe08d2b8245b82c5c5d
LePhanQuocVu <vulephanquoc@gmail.com>
2025-08-01 12:44:22 +0700

    connect to mqtt with SSL certificate

---
+++ include/Mqtt/mqtt.h
@@ -0,0 +1,30 @@
+#ifndef MQTT_H
+#define MQTT_H
+#include "../include/Wifi/wifi_connection.h"
+#include <PubSubClient.h>
+
+#define MSG_BUFFER_SIZE	(50)
+
+extern const char* mqtt_server;
+extern const char* mqtt_username;
+extern const char* mqtt_password;
+extern const int  mqtt_port;
+extern const char* root_ca;
+
+extern PubSubClient client;
+extern unsigned long lastMsg;
+
+extern char msg[MSG_BUFFER_SIZE];
+extern int value;
+
+void mqttInit();
+bool isMqttConnected();
+void callback(char* topic, byte* payload, unsigned int length);
+void reconnect();
+void recconnectIfNeed();
+void publicMessage(const char* topic, String payload,boolean retained);
+
+/**MQTT task */
+void MqttMonitorTask(void *pvParameter);
+
+#endif
