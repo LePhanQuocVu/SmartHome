c100c6fac455d038505a9ba41d347c441d24aaf2
LePhanQuocVu <vulephanquoc@gmail.com>
2025-09-02 16:46:09 +0700

    add global variable

--- include/global.h
+++ include/global.h
@@ -7,6 +7,9 @@
 
 #include "Mqtt/mqtt.h"
 #include "utils.h"
+#include "Servo/myServo.h"
+#include "Hcsr04/hcsr04.h"
+#include "Servo.h"
 
 /*PIN CONFIGURATION */
 #define LED_PIN 2
@@ -15,8 +18,8 @@
 #define DHTPIN 14 // D4 = GPIO14
 #define BUZZPIN 27 // D5 = GPIO27
 
-#define LED_HOME  25
-#define FAN_HOME 26
+/*SERVO */
+#define SERVO_PIN 12
 
 enum modeConnect {
   NORMAL,
@@ -24,8 +27,9 @@ enum modeConnect {
 };
 
 extern enum modeConnect currentMode;
-
+extern Servo myServo;
 extern SemaphoreHandle_t xMutex;
 extern QueueHandle_t xMqttQueue;
-
+extern QueueHandle_t xServoQueue;
+extern SemaphoreHandle_t xServoSemaphore;
 #endif
