b83ecf3b1791f7ca0e4b561e83a1616d2f2508e2
LePhanQuocVu <vulephanquoc@gmail.com>
2025-09-05 12:56:00 +0700

    create restfull api and connect to mongoDb compass

---
+++ back_end/src/index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const connectDB = require('./config/db')
+require('dotenv').config();
+const app = express()
+const port = 3000
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+app.use(cors());
+app.use(bodyParser.json());
+
+
+/** connect to db */
+connectDB();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
