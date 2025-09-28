a5f694f3aaeae477372cc32d6a36572add09f7ef
LePhanQuocVu <vulephanquoc@gmail.com>
2025-09-28 10:46:37 +0700

    add socket middleware for socketIO

--- src/services/socket.js
+++ src/services/socket.js
@@ -2,31 +2,63 @@ import { Server as IOServer } from 'socket.io';
 import jwt from 'jsonwebtoken';
 import User from '../model/userModel.js'
 import { socketAuthMiddleware } from '../middleware/socket.js';
+import userModel from '../model/userModel.js'; 
+import { publishToMqtt } from '../mqttBroker/mqtt.js';
 let ioInstance;
 
 export function initSocket(server) {
   const io = new IOServer(server, {
+    // cors: { origin: process.env.API_URL },
     cors: { origin: '*' },
     methods: ["GET", "POST"]
   });
   // middle ware
   io.use(socketAuthMiddleware);
-  
   // event khi kết nối
   io.on('connection', (socket) => {
     console.log('Socket connected:', socket.id, 'userId=', socket.user.id);
 
     socket.join(`user_${socket.user.id}`);
 
-    socket.on('hello', (msg) => {
-      console.log('hello from', socket.user.id, msg);
+    socket.on("toggledLed1", async(data) => {
+      console.log(`💡 User ${socket.user.id} toggled LED:`, data.state);
+     // đẩy lên MQTT
+      publishToMqtt("iot_home/led_state", JSON.stringify({
+        userId: socket.user.id,
+        state: data.state
+      }));
+
+      // Update Database
+      try{
+        const userId = socket.user.id;
+        const { state } = data;
+        // cập nhật DB cho user này
+        const user = await User.findOne({userId: userId});
+        if (!user) return;
+        user.ledState = state;
+        await user.save();
+      } catch(err) {
+         console.error("Error updating LED:", err.message);
+      }
+    })
+    socket.on("toggledLed2", async(data) => {
+       console.log(`💡 User ${socket.user.id} toggled LED2:`, data.state);
+    });
+    socket.on("toggledFan", async(data) => {
+        console.log(`💡 User ${socket.user.id} toggled Fan:`, data.state);
+         publishToMqtt("iot_home/fan_state", JSON.stringify({
+          userId: socket.user.id,
+          state: data.state
+        }));
+    });
+    socket.on("changeMode", async(data) => {
+        console.log(`💡 User ${socket.user.id} Workmode:`, data.state);
     });
 
     socket.on('disconnect', (reason) => {
       console.log('Socket disconnected', socket.id, reason);
     });
   });
-
   ioInstance = io;
   return io;
 }
